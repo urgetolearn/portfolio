31f5b09ec4d010c7ffd563bf6f1b18e119058db9
Chinmayi D S <chinmayishaan2316@gmail.com>
2025-11-05 03:17:01 +0530

    edits made

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -6,7 +6,6 @@ const Education = () => {
     <section
       id="education"
       className="w-full py-24 px-4 md:px-8 lg:px-16 font-sans bg-skills-gradient">
-      {/* Section Title */}
       <div className="text-center mb-16">
         <h2 className="text-4xl font-bold text-white">EDUCATION</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
@@ -17,8 +16,8 @@ const Education = () => {
       </div>
 
       <div className="relative">
-        {/* Vertical line only on medium+ screens */}
-        <div className="hidden sm:block absolute left-1/2 transform -translate-x-1/2 w-1 bg-white h-full"></div>
+        {/* Vertical line for medium+ screens */}
+        <div className="hidden sm:block absolute left-1/2 top-0 transform -translate-x-1/2 w-1 bg-white h-full"></div>
 
         {education.map((edu, index) => (
           <div
@@ -26,8 +25,8 @@ const Education = () => {
             className={`flex flex-col sm:flex-row items-start sm:items-center mb-16 relative ${
               index % 2 === 0 ? "sm:justify-start" : "sm:justify-end"
             }`}>
-            {/* Bubble only on medium+ screens */}
-            <div className="hidden sm:flex absolute left-1/2 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-12 h-12 sm:w-16 sm:h-16 rounded-full justify-center items-center z-10">
+            {/* Bubble/logo */}
+            <div className="hidden sm:flex absolute left-1/2 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-16 h-16 rounded-full justify-center items-center z-10">
               <img
                 src={edu.img}
                 alt={edu.school}
@@ -38,8 +37,8 @@ const Education = () => {
             {/* Card */}
             <div
               className={`w-full sm:max-w-md p-4 sm:p-8 rounded-2xl shadow-2xl border border-white bg-gray-900 backdrop-blur-md shadow-[0_0_20px_1px_rgba(130,69,236,0.3)] ${
-                index % 2 === 0 ? "sm:ml-0 sm:mr-12" : "sm:mr-0 sm:ml-12"
-              } ml-0 mr-0 sm:ml-4 sm:mr-4 transform transition-transform duration-300 hover:scale-105`}>
+                index % 2 === 0 ? "sm:ml-8 sm:mr-auto" : "sm:mr-8 sm:ml-auto"
+              } transform transition-transform duration-300 hover:scale-105`}>
               <div className="flex items-center space-x-4">
                 <div className="w-20 h-20 bg-white rounded-full overflow-hidden">
                   <img
